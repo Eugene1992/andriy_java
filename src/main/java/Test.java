9616065acac278d78a0cc71e7e5f9a2606b2525c
Yevhenii Deineka <yevhenii.deineka@netcracker.com>
2022-03-16 20:51:51 +0100

    Added some files

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,7 @@ public class Test {
 		System.out.println("Hello world!");
 		System.out.println("Hello world!");
 		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
 	}
 }
